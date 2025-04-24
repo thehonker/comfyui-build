3be0a222bbaf7c9dcb2bec03bc785bdc320eb604
Edwin Pers <pers.edwin@gmail.com>
2025-04-23 20:46:05 -0400

    dotsource

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,6 +4,6 @@ echo "entrypoint.sh start"
 
 cd "${HOME}/comfyui/" || exit 1
 
-source "${HOME}/comfyui/bin/activate"
+. "${HOME}/comfyui/bin/activate"
 
 exec python3 "${HOME}/comfyui/main.py" "$@"
