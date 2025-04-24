700d06ea8e19e914ec2b6ac915b75664b0d5341a
Edwin Pers <pers.edwin@gmail.com>
2025-04-23 21:47:57 -0400

    make entrypoint more verbose

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,8 +2,12 @@
 
 echo "entrypoint.sh start"
 
+echo "activating venv at ${HOME}/comfyui"
+
 cd "${HOME}/comfyui/" || exit 1
 
 . "${HOME}/comfyui/bin/activate"
+
+echo "launching comfyui with args: $@"
 
 exec python3 "${HOME}/comfyui/main.py" "$@"
