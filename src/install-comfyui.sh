248285b0c11e14018dc8261e93194ff62503e3aa
Edwin Pers <pers.edwin@gmail.com>
2025-10-15 20:21:04 -0400

    we don't really need to rm the installers

--- src/install-comfyui.sh
+++ src/install-comfyui.sh
@@ -14,5 +14,3 @@ $HOME/.local/bin/uv venv --relocatable --prompt invoke --python 3.12 --python-pr
 $HOME/.local/bin/uv pip install --upgrade torch torchvision torchaudio --python 3.12 --python-preference only-managed --torch-backend=$GPU_DRIVER --force-reinstall
 $HOME/.local/bin/uv pip install -r $HOME/comfyui/requirements.txt
 deactivate
-
-rm /install-comfyui.sh
